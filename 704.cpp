77ca470e16f4ef9e613c4855a56a0f63a20c7c4e
YashChauhan-2303 <yashchauhan.2303@gmail.com>
2025-05-24 16:26:01 +0530

    704.cpp

---
+++ 704.cpp
@@ -0,0 +1,27 @@
+//704. Binary Search
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int search(vector<int>& nums, int target) {
+    int left=0;
+    int right=nums.size()-1;
+    while(left<=right){
+        int mid = (left+right)/2;
+        if(nums[mid]<target){
+            left=mid+1;
+        } else if (nums[mid] > target){
+            right = mid-1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    vector<int> nums = { -1,0,3,5,9,12 };
+    int target = 9;
+    cout << search(nums,target);
+    return 0;
+}

e487b0122dae039a9a8a5b722a1c894e2980a666
YashChauhan-2303 <yashchauhan.2303@gmail.com>
2025-05-27 13:11:41 +0530

    2894.cpp

--- 2894.cpp
+++ 2894.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int differenceOfSums(int n, int m) {
     int sum1 =0;
     int sum2 =0;
-    for(int i=0;i<=n;i++){
+    for(int i=1;i<=n;i++){
         if(i%m==0){
             sum1 += i;
         } else {

fcd7ecdba6529ad9aaf1e09ebb7b61cae48f29d3
YashChauhan-2303 <yashchauhan.2303@gmail.com>
2025-05-27 13:08:36 +0530

    2894.cpp

---
+++ 2894.cpp
@@ -0,0 +1,24 @@
+//2894. Divisible and Non-divisible Sums Difference
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int differenceOfSums(int n, int m) {
+    int sum1 =0;
+    int sum2 =0;
+    for(int i=0;i<=n;i++){
+        if(i%m==0){
+            sum1 += i;
+        } else {
+            sum2 += i;
+        }
+    }
+    return sum2-sum1;
+}
+
+int main(){
+    int n = 10;
+    int m = 3;
+    cout << differenceOfSums(n,m);
+    return 0;
+}

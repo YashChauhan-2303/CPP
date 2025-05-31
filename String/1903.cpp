bade05415477014efd04074cf7bbc33d5616536d
YashChauhan-2303 <yashchauhan.2303@gmail.com>
2025-05-31 19:06:45 +0530

    1903.cpp

--- String/1903.cpp
+++ String/1903.cpp
@@ -6,11 +6,16 @@
 using namespace std;
 
 string largestOddNumber(string num) {
-    
+    for(int i=num.size()-1; i>=0; i--){
+        if ((num[i]-'0')%2==1){
+            return num.substr(0,i+1);
+        }
+    }
+    return "";
 }   
 
 int main(){
-    string num = "2";
+    string num = "52";
     cout << largestOddNumber(num);
     return 0;
 }

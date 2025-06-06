fafeb9ff44842f73b2f974edf67399cac636e319
YashChauhan-2303 <yashchauhan.2303@gmail.com>
2025-06-06 22:57:32 +0530

    231.cpp

---
+++ BS 1D Array/231.cpp
@@ -0,0 +1,20 @@
+//231. Power of Two
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isPowerOfTwo(int n) {
+    if(n<=0) return 0;
+    int x = n;
+    while(x%2==0){
+        x /= 2;
+    }
+    if (x==1) return 1;
+    return 0;
+}
+
+int main(){
+    int n = 3;
+    cout << isPowerOfTwo(n);
+    return 0;
+}

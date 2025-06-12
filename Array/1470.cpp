0ced2d7524a70c618a88ac4b88312e2aac48ec7d
YashChauhan-2303 <yashchauhan.2303@gmail.com>
2025-06-12 17:59:31 +0530

    1470.cpp

---
+++ Array/1470.cpp
@@ -0,0 +1,17 @@
+//1470. Shuffle the Array
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        vector<int> ans;
+        int j = n;
+        int i = 0;
+        while(j<2*n){
+            ans.push_back(nums[i]);
+            ans.push_back(nums[j]);
+            i++;
+            j++;
+        }
+        return ans;
+    }
+};

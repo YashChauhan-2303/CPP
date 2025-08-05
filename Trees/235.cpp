be465be84f846a7f747d9497c5152c407f36ff1d
YashChauhan-2303 <yashchauhan.2303@gmail.com>
2025-08-05 15:53:24 +0530

    235.cpp

---
+++ Trees/235.cpp
@@ -0,0 +1,20 @@
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+//  * };
+//  */
+
+// class Solution {
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         if(root == nullptr) return root;
+//         int val = root->val;
+//         if(val < p->val && val < q->val) return lowestCommonAncestor(root->right,p,q);
+//         if(val > p->val && val > q->val) return lowestCommonAncestor(root->left,p,q);
+//         return root;
+//     }
+// };

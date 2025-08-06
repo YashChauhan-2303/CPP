337b91d0e8eea6f5b7e21e1f7a695515db81cba4
YashChauhan-2303 <yashchauhan.2303@gmail.com>
2025-08-06 16:01:39 +0530

    70.cpp

--- DP/70.cpp
+++ DP/70.cpp
@@ -2,9 +2,23 @@
 // public:
 //     int climbStairs(int n) {
 //         if(n == 0) return 1;
-//         if(n == 1) return 0;
+//         if(n == 1) return 1;
 //         int left = climbStairs(n-1);
 //         int right = climbStairs(n-2);
 //         return left+right;
+//     }
+// };
+
+// class Solution {
+// public:
+//     int helper(int n, vector<int>& dp) {
+//         if(n < 0) return 0;
+//         if(n == 0 || n == 1) return 1;
+//         if(dp[n] != -1) return dp[n];
+//         return dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
+//     }
+//     int climbStairs(int n) {
+//         vector<int> dp(n + 1, -1);
+//         return helper(n, dp);
 //     }
 // };

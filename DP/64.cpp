473fb9bcc9ab18874ec623610d95cfbceee9bcf1
YashChauhan-2303 <yashchauhan.2303@gmail.com>
2025-08-11 16:02:26 +0530

    64.cpp

---
+++ DP/64.cpp
@@ -0,0 +1,19 @@
+// class Solution {
+// public:
+//     int checkSum(vector<vector<int>>& grid, int m, int n,vector<vector<int>>& dp){
+//         if(m==0 && n==0) return grid[0][0];
+//         if(m<0 || n<0) return INT_MAX;
+//         if(dp[m][n] != -1) return dp[m][n];
+//         int left = checkSum(grid,m,n-1,dp) ;
+//         int up = checkSum(grid,m-1,n,dp) ;
+//         return dp[m][n] = grid[m][n] + min(left,up);
+//     }
+
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         vector<vector<int>> dp(m,vector<int>(n,-1));
+//         checkSum(grid,m-1,n-1,dp);
+//         return checkSum(grid, m - 1, n - 1, dp);
+//     }
+// };

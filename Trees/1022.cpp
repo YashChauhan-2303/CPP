bb6632e7c611f4564c221bc67c49544e9f92d059
YashChauhan-2303 <yashchauhan.2303@gmail.com>
2026-02-24 19:00:05 +0530

    Trees/1022.cpp

---
+++ Trees/1022.cpp
@@ -0,0 +1,30 @@
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     int preorder(TreeNode* root,int sum){
+//         if(root == NULL)    return 0;
+//         sum = sum<<1;
+//         sum |= root->val;
+//         if(root->left == NULL && root->right == NULL)   return sum;
+//         int a = preorder(root->left,sum);
+//         int b = preorder(root->right,sum);
+//         return a+b;
+//     }
+
+//     int sumRootToLeaf(TreeNode* root) {
+//         int sum = 0;
+//         sum = preorder(root,0);
+//         return sum;
+//     }
+
+// };
